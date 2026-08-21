9e7988055d014874d12c738a3bf8a5f648edfceb
Roman D <roman.deko.history@gmail.com>
2026-08-21 21:18:51 +0700

    875tg

--- scr.js
+++ scr.js
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     function initSwiper() {
         const defaultSlideIndex = slidesData.findIndex(function (slide) {
-            return slide.tag === '874';
+            return slide.tag === '875';
         });
         const isMobile = window.matchMedia('(max-width: 768px)').matches;
 
